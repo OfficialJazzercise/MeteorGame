3c95fbc7c4dfe2f5a6bbb2a0c631e59a7b913ec7
Lynkus2thepast <46943942+Lynkus2thepast@users.noreply.github.com>
2021-01-19 19:49:26 -0600

    spawner stuff almost done

---
+++ Assets/SpaceRock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceRock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+    
+    void Start()
+    {
+       
+        
+    }
+
+    IEnumerator RockDrop()
+    {
+        while (enemyCount < 1)
+        {
+            xPos = Random.Range(/*we will get this later*/);
+            zPos = Random.Range(/*same here*/);
+            Instantiate(theEnemy, new Vector3(xPos,/*the Y Coords*/, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);// sets the spawn rate
+            enemyCount += 1; //limit of rocks
+
+        }
+    }
+}
