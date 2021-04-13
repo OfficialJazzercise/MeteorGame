377ff3ec411fa547f849044c24e85acd7a635074
MastrDarkStar <46503057+MastrDarkStar@users.noreply.github.com>
2021-04-13 16:40:22 -0500

    VFX and Post Processing update

---
+++ Assets/Scripts/Meteor and Enemy Scripts/SelfDestruct.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public GameObject Thingy;
+    int x = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(x < 40)
+            x = x + 1;
+        else if(x >= 40)
+            Destroy(Thingy);
+    }
+}
