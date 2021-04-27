b3f7db51f22c13c75dad790e8688b69394892303
Daylight, the Forbidden PSI <46943944+psiDaylight@users.noreply.github.com>
2021-04-26 21:56:35 -0500

    High Scores Added

---
+++ Assets/Scripts/Menu Scripts/HighscoreTable.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HighscoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private List<Transform> highscoreEntryTransformList;
+    private const int highscoreLimit = 10;
+ 
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        AddHighscoreEntry(6, "FML");
+        
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+
+        highscoreEntryTransformList = new List<Transform>();
+        foreach(HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+        
+    }
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 65f;
+
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count + 1;
+        string rankString;
+
+        switch (rank)
+        {
+            case 1:
+                rankString = "1ST";
+                break;
+
+            case 2:
+                rankString = "2ND";
+                break;
+
+            case 3:
+                rankString = "3RD";
+                break;
+            default:
+                rankString = rank + "TH";
+                break;
+        }
+
+        entryTransform.Find("posText").GetComponent<TextMeshProUGUI>().SetText(rankString);
+
+        int score = highscoreEntry.score;
+
+        entryTransform.Find("scoreText").GetComponent<TextMeshProUGUI>().SetText(score.ToString());
+
+        string name = highscoreEntry.name;
+
+        entryTransform.Find("nameText").GetComponent<TextMeshProUGUI>().SetText(name);
+
+        transformList.Add(entryTransform);
+    }
+
+    private void AddHighscoreEntry(int score, string name)
+    {
+        HighscoreEntry highscoreEntry = new HighscoreEntry {score = score, name = name };
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        highscores.highscoreEntryList.Add(highscoreEntry);
+
+        for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+        {
+            for (int j = 0; j < highscores.highscoreEntryList.Count; j++)
+            {
+                if (highscores.highscoreEntryList[j].score < highscores.highscoreEntryList[i].score)
+                {
+                    HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                    highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                    highscores.highscoreEntryList[j] = tmp;
+                }
+            }
+        }
+
+        while (highscores.highscoreEntryList.Count > highscoreLimit)
+        {
+            highscores.highscoreEntryList.RemoveAt(highscores.highscoreEntryList.Count - 1);
+        }
+
+        string json = JsonUtility.ToJson(highscores);
+
+
+        //Use this as a template to determine player scores and which scene to reset to
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+    }
+
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+
+    [System.Serializable]
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
+    }
+}
