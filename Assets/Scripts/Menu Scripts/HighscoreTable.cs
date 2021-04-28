706912348eda8b152c029e0d1fd9d8744a1f79f4
Daylight, the Forbidden PSI <46943944+psiDaylight@users.noreply.github.com>
2021-04-28 14:59:01 -0500

    Scoreboard Semi-Functioning

--- Assets/Scripts/Menu Scripts/HighscoreTable.cs
+++ Assets/Scripts/Menu Scripts/HighscoreTable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class HighscoreTable : MonoBehaviour
 {
@@ -11,6 +12,14 @@ public class HighscoreTable : MonoBehaviour
     private List<Transform> highscoreEntryTransformList;
     private const int highscoreLimit = 10;
 
+    private List<string> alphabet;
+    private string inputName;
+    private int counter = 3;
+
+    private int currentLetter = 0;
+
+    private IEnumerator coroutine;
+
     private const string defaultPref = "{\"highscoreEntryList\":[{\"score\":53488,\"name\":\"AAA\"},{\"score\":58888,\"name\":\"FML\"}]}";
  
 
@@ -18,21 +27,13 @@ public class HighscoreTable : MonoBehaviour
     {
         entryContainer = transform.Find("highscoreEntryContainer");
         entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+        alphabet = new List<string>();
+        makeAlphabet();
 
         entryTemplate.gameObject.SetActive(false);
 
-        AddHighscoreEntry(6, "FML");
-        
-        string jsonString = PlayerPrefs.GetString("highscoreTable", defaultPref);
-        Debug.Log(jsonString);
-        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
-
-
-        highscoreEntryTransformList = new List<Transform>();
-        foreach(HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
-        {
-            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
-        }
+        coroutine = AddP1Score();
+        StartCoroutine(coroutine);
         
     }
 
@@ -79,12 +80,104 @@ public class HighscoreTable : MonoBehaviour
         transformList.Add(entryTransform);
     }
 
-    private void AddHighscoreEntry(int score, string name)
+    private class Highscores
     {
-        HighscoreEntry highscoreEntry = new HighscoreEntry {score = score, name = name };
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+
+    [System.Serializable]
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
+    }
+
+    private void makeAlphabet()
+    {
+        alphabet.Add("A");
+        alphabet.Add("B");
+        alphabet.Add("C");
+        alphabet.Add("D");
+        alphabet.Add("E");
+        alphabet.Add("F");
+        alphabet.Add("G");
+        alphabet.Add("H");
+        alphabet.Add("I");
+        alphabet.Add("J");
+        alphabet.Add("K");
+        alphabet.Add("L");
+        alphabet.Add("M");
+        alphabet.Add("N");
+        alphabet.Add("O");
+        alphabet.Add("P");
+        alphabet.Add("Q");
+        alphabet.Add("R");
+        alphabet.Add("S");
+        alphabet.Add("T");
+        alphabet.Add("U");
+        alphabet.Add("V");
+        alphabet.Add("W");
+        alphabet.Add("X");
+        alphabet.Add("Y");
+        alphabet.Add("Z");
+    }
+
+    public void scrollUp(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            currentLetter++;
+
+            if (currentLetter >= 26)
+            {
+                currentLetter = 0;
+            }
+        }
+    }
+
+    public void scrollDown(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            currentLetter--;
+
+            if (currentLetter < 0)
+            {
+                currentLetter = 25;
+            }
+        }
+    }
+
+    public void confirm(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            inputName += alphabet[currentLetter];
+            currentLetter = 0;
+            counter--;   
+        }
+    }
+
+    private IEnumerator AddP1Score()
+    {
 
         string jsonString = PlayerPrefs.GetString("highscoreTable", defaultPref);
         Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+
+        if (highscores.highscoreEntryList[highscores.highscoreEntryList.Count - 1].score < PlayerPrefs.GetInt("P1Score", 12000))
+        {
+            counter = 3;
+            inputName = "";
+
+            while (counter > 0)
+            {
+                yield return null;
+            }
+        }
+
+        HighscoreEntry highscoreEntry = new HighscoreEntry { score = PlayerPrefs.GetInt("P1Score", 12000), name = inputName };
 
         highscores.highscoreEntryList.Add(highscoreEntry);
 
@@ -101,6 +194,7 @@ public class HighscoreTable : MonoBehaviour
             }
         }
 
+
         while (highscores.highscoreEntryList.Count > highscoreLimit)
         {
             highscores.highscoreEntryList.RemoveAt(highscores.highscoreEntryList.Count - 1);
@@ -112,18 +206,71 @@ public class HighscoreTable : MonoBehaviour
         //Use this as a template to determine player scores and which scene to reset to
         PlayerPrefs.SetString("highscoreTable", json);
         PlayerPrefs.Save();
+
+        coroutine = AddP2Score();
+        StartCoroutine(coroutine);
     }
 
-    private class Highscores
+    private IEnumerator AddP2Score()
     {
-        public List<HighscoreEntry> highscoreEntryList;
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable", defaultPref);
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        if (highscores.highscoreEntryList[highscores.highscoreEntryList.Count - 1].score < PlayerPrefs.GetInt("P2Score", 12000))
+        {
+            counter = 3;
+            inputName = "";
+
+            while (counter > 0)
+            {
+                yield return null;
+            }
+        }
+
+        HighscoreEntry highscoreEntry = new HighscoreEntry { score = PlayerPrefs.GetInt("P2Score", 12000), name = inputName };
+
+        highscores.highscoreEntryList.Add(highscoreEntry);
+
+        for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+        {
+            for (int j = 0; j < highscores.highscoreEntryList.Count; j++)
+            {
+                if (highscores.highscoreEntryList[j].score < highscores.highscoreEntryList[i].score)
+                {
+                    HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                    highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                    highscores.highscoreEntryList[j] = tmp;
+                }
+            }
+        }
+
+
+        while (highscores.highscoreEntryList.Count > highscoreLimit)
+        {
+            highscores.highscoreEntryList.RemoveAt(highscores.highscoreEntryList.Count - 1);
+        }
+
+        string json = JsonUtility.ToJson(highscores);
+
+
+        //Use this as a template to determine player scores and which scene to reset to
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+
+        makeScoreBoard();
     }
 
-
-    [System.Serializable]
-    private class HighscoreEntry
+    private void makeScoreBoard()
     {
-        public int score;
-        public string name;
+        string jsonString = PlayerPrefs.GetString("highscoreTable", defaultPref);
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+
+        highscoreEntryTransformList = new List<Transform>();
+        foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
     }
 }
